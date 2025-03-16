c9339a037a01cd42ae33759b5b3f34e5394baf13
Hunter <warsvitaly@gmail.com>
2025-03-16 14:42:54 +0800

    add new fun

--- game.js
+++ game.js
@@ -41,6 +41,10 @@ export class Game {
 
   start() {
     this.initializeControls();
+
+    this.score = this.getCookie('score')
+    console.log("score " + this.score);
+
     this.intervalId = setInterval(() => this.draw(), 10);
   
     this.canvas.addEventListener('click', (e) => {
@@ -113,11 +117,17 @@ export class Game {
 		this.bird = new Bird(this.canvas);
 		this.pipes = [new Pipe(this.canvas)];
 		this.ground = new Ground(this.canvas);
+
+    this.setCookie('score', this.score, {secure: true, 'max-score': 3600});
+    console.log("score " + this.score )
+
 		this.score = 0;
 		this.frameCount = 0;
 		this.k = 3.5;
 		this.DISTANCE_BETWEEN_PIPES = this.k * Pipe.width;
 		this.SPEED = 3;
+
+    
   }
 
   updatePipes() {
@@ -146,4 +156,36 @@ export class Game {
     if (!this.isGameStarted) this.isGameStarted = true;
     this.bird.flap();
   }  
+
+getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+  setCookie(name, value, options = {}) {
+    options = {
+      path: '/',
+      // при необходимости добавьте другие значения по умолчанию
+      ...options
+    };
+  
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+  
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+  
+    document.cookie = updatedCookie;
+  }
+
 }
